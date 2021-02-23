f4ff11be33cff02b7e42cf147f90092344b80f4c
Brandyn Abel <brandynabel@gmail.com>
2021-02-23 13:26:25 -0600

    Address case sensitivity

--- src_shared/getLink.js
+++ src_shared/getLink.js
@@ -1,5 +1,5 @@
 function getLink(srcUrl) {
-    let regx = new RegExp("(?<=_).*\.(jpg|png)$");
+    let regx = new RegExp("(?<=_).*\.(jpg|png)$","ig");
     let link = `[${srcUrl.match(regx)[0].replaceAll("_","-")}]`
     console.log('Generated Link: ', link, 'From: ', srcUrl);
     return link;
