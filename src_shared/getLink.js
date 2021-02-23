cdbf09e0cf6cb7423fd1fb5815569a8182a6b10f
Brandyn Abel <brandynabel@gmail.com>
2021-02-23 13:06:25 -0600

    Address issue in link generation for .png or contain _:

--- src_shared/getLink.js
+++ src_shared/getLink.js
@@ -1,7 +1,6 @@
 function getLink(srcUrl) {
-    let regx = new RegExp("(?<=_).*\.jpg");
-    let link = `[${srcUrl.match(regx)[0]}]`
-
+    let regx = new RegExp("(?<=_).*\.(jpg|png)$");
+    let link = `[${srcUrl.match(regx)[0].replaceAll("_","-")}]`
     console.log('Generated Link: ', link, 'From: ', srcUrl);
     return link;
 }
