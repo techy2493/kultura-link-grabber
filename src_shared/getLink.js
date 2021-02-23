b70919d71a682375faab0f38ea6db470c2031120
Brandyn Abel <brandynabel@gmail.com>
2021-02-23 13:28:53 -0600

    Prompt reporting of non-matching images

--- src_shared/getLink.js
+++ src_shared/getLink.js
@@ -1,6 +1,10 @@
 function getLink(srcUrl) {
     let regx = new RegExp("(?<=_).*\.(jpg|png)$","ig");
-    let link = `[${srcUrl.match(regx)[0].replaceAll("_","-")}]`
+    let match = srcUrl.match(regx)[0];
+    if (!match) {
+        alert("Sorry we couldn't get this link, please file an issue at https://github.com/techy2493/kultura-link-grabber/issues and we'll get it fixed as soon as possible!")
+    }
+    let link = `[${match.replaceAll("_","-")}]`
     console.log('Generated Link: ', link, 'From: ', srcUrl);
     return link;
 }
