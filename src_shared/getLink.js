1bf44847635840f2b28206bbf46d6852ccf73f05
Brandyn Abel <brandynabel@gmail.com>
2021-02-23 23:04:19 -0600

    add jpeg file type and address issue with match error message

--- src_shared/getLink.js
+++ src_shared/getLink.js
@@ -1,11 +1,11 @@
 function getLink(srcUrl) {
-    let regx = new RegExp("(?<=_).*\.(jpg|png)$","ig");
-    let match = srcUrl.match(regx)[0];
-    if (!match) {
+    let regx = new RegExp("(?<=_).*\.(jpg|png|jpeg)$","ig");
+    let matches = srcUrl.match(regx);
+    if (!matches) {
         alert("Sorry we couldn't get this link, please file an issue at https://github.com/techy2493/kultura-link-grabber/issues and we'll get it fixed as soon as possible!")
         return;
     }
-    let link = `[${match.replaceAll("_","-")}]`
+    let link = `[${matches[0].replaceAll("_","-")}]`
     console.log('Generated Link: ', link, 'From: ', srcUrl);
     return link;
 }
